dbe15ecf531609476bd34949068972be19d06dcf
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-11-17 08:32:20 -0800

    service-worker files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,19 @@
 console.log("Hi from your service-worker.js file!");
 
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+//   '/',
+//   '/index.html',
+//   '/favicon.ico',
+//   '/manifest.webmanifest',
+//   '/assets/css/style.css',
+//   '/assets/js/loadImages.js',
+//   '/assets/images/icons/icon-72x72.png',
+  
+];
+
 // install
 self.addEventListener("install", function (evt) {
     // pre cache image data
@@ -35,5 +49,31 @@ self.addEventListener("install", function (evt) {
   });
 
   self.addEventListener('fetch', function(evt) {
-    // code to handle requests goes here
+    if (evt.request.url.includes('/api')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+        )
+    }
     });
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
