77b61ed9e07febe29aa0006916952cbbb3605928
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-11-18 11:25:58 -0800

    server.js final updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,13 +4,12 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
-//   '/',
-//   '/index.html',
-//   '/favicon.ico',
-//   '/manifest.webmanifest',
-//   '/assets/css/style.css',
-//   '/assets/js/loadImages.js',
-//   '/assets/images/icons/icon-72x72.png',
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/styles.css',
+  '/public/icons/icon-192x192.png',
+  '/public/icons/icon-512x512.png',
   
 ];
 
